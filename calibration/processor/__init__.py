a7875003700a59c2910cfd3e14bf81acefba7cfe
Ebad Kamil <ebad.kamil@xfel.eu>
2019-11-25 16:39:16 +0100

    Imports in __init__

--- calibration/processor/__init__.py
+++ calibration/processor/__init__.py
@@ -1,10 +1,10 @@
 from .data_processing import DataProcessing, DataModel, eval_statistics, gauss_fit
-from .operations import eval_histogram
+from .operations import EvalHistogram
 
 __all__ = [
     "DataProcessing",
     "DataModel",
     "eval_statistics",
     "gauss_fit",
-    "eval_histogram"
+    "EvalHistogram"
     ]
