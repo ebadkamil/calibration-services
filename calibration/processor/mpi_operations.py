1f99fa26fc5aa2a6bb6e4383e3d982c5a4d3f724
Ebad Kamil <ebad.kamil@xfel.eu>
2019-11-26 14:16:38 +0100

    Add multihistogram in MPI class

--- calibration/processor/mpi_operations.py
+++ calibration/processor/mpi_operations.py
@@ -26,15 +26,42 @@ from .MPI import distribute
 
 
 class MPIEvalHistogram:
+     """MPI Class to evaluate histogram
+
+    Attributes:
+    -----------
+    path: (str) Path to Run folder
+    dettype: (str) AGIPD, LPD
+    pixel_hist: (bool) optional
+        Default: False. For pixel wise histogram set it to True
+    dark_run: (numpy.ndarray) optional
+        dark_dta shape (n_pulses, slow_scan, fast_scan)
+        Default: None,
+        If provided dark data will be subtracted from images"""
 
     def __init__(self, path,
                  pixel_hist=False, comm=MPI.COMM_WORLD, dark_run=None):
+        """Initialization"""
         self.comm = comm
         self.path = path
         self.pixel_hist = pixel_hist
         self.dark_run = dark_run
 
+        self.modno = None
+        self.histogram = None
+
     def process(self, bin_edges, pulse_ids=None, workers=None):
+        """Evaluate Histogram and mean image
+        Parameters:
+        -----------
+            bin_edges: (np.1darray) required
+            pulse_ids: str, optional
+                Default: all pulses ":"
+                For eg. ":" to select all pulses in a train
+                "start:stop:step" to select indices with certain step size
+                "1,2,3" comma separated pulse index to select specific pulses
+                "1,2,3, 5:10" mix of above two
+        """
         self.bin_edges = bin_edges
         pulse_ids = ":" if pulse_ids is None else pulse_ids
         self.pulses = parse_ids(pulse_ids)
@@ -42,16 +69,13 @@ class MPIEvalHistogram:
         self.channels, self.local_sequences = distribute(
             self.path, comm=self.comm)
 
-        print(self.local_sequences)
-
-        self.modno, self.histogram = self.eval_histogram()
+        self.eval_histogram()
         self.gather()
 
     def gather(self):
 
         if self.comm.rank == 0:
             self.result = {mod: 0 for mod in self.channels}
-            self.bin_centers = (self.bin_edges[1:] + self.bin_edges[:-1]) / 2.0
 
             if self.modno is not None:
                 self.result[self.modno] += self.histogram
@@ -59,8 +83,10 @@ class MPIEvalHistogram:
             for source in range(1, self.comm.size):
                 mod = self.comm.recv(source=source)
                 if mod is not None:
-                    histogram = np.empty(
-                        (len(self.pulses),) + bin_centers.shape, dtype=np.int64)
+                    shape = self.comm.recv(source=source)
+                    dtype = self.comm.recv(source=source)
+                    print("Recv ", source, shape, dtype)
+                    histogram = np.empty(shape, dtype=dtype)
                     self.comm.Recv(histogram, source=source)
                 else:
                     histogram = self.comm.recv(source=source)
@@ -69,7 +95,13 @@ class MPIEvalHistogram:
                     self.result[mod] += histogram
         else:
             self.comm.send(self.modno, dest=0)
+
             if self.modno is not None:
+                shape = self.histogram.shape
+                dtype = self.histogram.dtype
+                print("Send ", self.comm.rank, shape, dtype)
+                self.comm.send(shape, dest=0)
+                self.comm.send(dtype, dest=0)
                 self.comm.Send(self.histogram, dest=0)
             else:
                 self.comm.send(self.histogram, dest=0)
@@ -77,11 +109,11 @@ class MPIEvalHistogram:
     def eval_histogram(self):
 
         if not self.local_sequences:
-            return None, None
+            return
 
         modno, files = zip(*self.local_sequences)
 
-        run = DataCollection.from_paths(list(files))
+        run = DataCollection.from_paths(list(files)).select_trains(by_index[:20])
 
         module = [key for key in run.instrument_sources
                   if re.match(r"(.+)/DET/(.+):(.+)", key)]
@@ -129,12 +161,10 @@ class MPIEvalHistogram:
 
             else:
                 """Evaluate histogram over each pixel"""
-                def multihist(chunk, data, bin_edges, ret):
-                    start, end = chunk
-                    temp = data[:, start:end, :]
-                    bin_ix = np.searchsorted(bin_edges[1:], temp)
+                def multihist(data, bin_edges):
+                    bin_ix = np.searchsorted(bin_edges[1:], data)
 
-                    X, Y, Z = temp.shape
+                    X, Y, Z = data.shape
                     xgrid, ygrid, zgrid = np.meshgrid(
                         np.arange(X),
                         np.arange(Y),
@@ -144,48 +174,31 @@ class MPIEvalHistogram:
                     counts = np.zeros((X, Y, Z, len(bin_edges)), dtype=np.uint32)
 
                     np.add.at(counts, (xgrid, ygrid, zgrid, bin_ix), 1)
-                    ret[:, start:end, :, :] = counts[..., :-1]
+                    return counts[..., :-1]
 
-                counts = np.zeros(
-                    (len(self.pulses),
-                    512,
-                    128,
-                    len(self.bin_edges)-1),
-                    dtype=np.uint32)
-
-                start = 0
-                chunk_size = 32
-                chunks = []
-
-                while start < counts.shape[1]:
-                    chunks.append(
-                        (start, min(start + chunk_size, counts.shape[1])))
-                    start += chunk_size
-
-                with ThreadPoolExecutor(max_workers=16) as executor:
-                    for chunk in chunks:
-                        executor.submit(
-                            multihist, chunk, image, self.bin_edges, counts)
-
+                counts = multihist(image, self.bin_edges)
                 histogram += counts
 
             num_trains += 1
+
         if num_trains != 0:
-            return modno[0], histogram
-        else:
-            return None, None
+            self.modno = modno[0]
+            self.histogram = histogram
 
     def hist_to_file(self, path):
         if path and self.comm.rank == 0:
+            bin_centers = (self.bin_edges[1:] + self.bin_edges[:-1]) / 2.0
+
             with h5py.File(path, "w") as f:
                 for modno, data in self.result.items():
                     g = f.create_group(f"entry_1/instrument/module_{modno}")
                     g.create_dataset('counts', data=data)
-                    g.create_dataset('bins', data=self.bin_centers)
+                    g.create_dataset('bins', data=bin_centers)
 
 
 if __name__ == "__main__":
     path = "/gpfs/exfel/exp/MID/201901/p002542/raw/r0349"
-    e = MPIEvalHistogram(path)
-    bin_edges = np.linspace(0, 10000, 1001, dtype=np.float32)
+    e = MPIEvalHistogram(path, pixel_hist=True)
+    bin_edges = np.linspace(0, 10000, 601, dtype=np.float32)
     e.process(bin_edges, pulse_ids='0:20:1')
+    e.hist_to_file('/gpfs/exfel/data/scratch/kamile/calibration_analysis/all_counts_mpi_class.h5')
