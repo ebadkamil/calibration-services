17a6dfe536d9ddd800d1cd795b1d2a0bb83f884a
Ebad Kamil <ebad.kamil@xfel.eu>
2020-04-24 13:38:08 +0200

    Add AgipdAssembler class in ImageAssembler factory

--- calibration/processor/assembler.py
+++ calibration/processor/assembler.py
@@ -46,6 +46,51 @@ class ImageAssembler(object):
 
             return assembled
 
+    class AgipdAssembler(BaseAssembler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def get_geom_object(self):
+            if self.geom_file is not None:
+                try:
+                    self.geom = AGIPD_1MGeometry.from_crystfel_geom(
+                        self.geom_file)
+                except Exception as ex:
+                    print(ex)
+            else:
+                self.geom = AGIPD_1MGeometry.from_quad_positions(
+                    quad_pos=[
+                        (-525, 625),
+                        (-550, -10),
+                        (520, -160),
+                        (542.5, 475), ])
+
+        def _get_modules_data(self, train_data, dark_data=None):
+
+            def _corrections(source, train_data=train_data):
+                pattern = "(.+)/DET/(.+)CH0:xtdf"
+                modno = int((re.match(pattern, source)).group(2).strip())
+
+                image = train_data[source]["image.data"][:, 0, ...]
+
+                image = image.astype(np.float32)
+
+                if dark_data is not None and image.shape[0] != 0:
+                    image -= dark_data[str(modno)][0:image.shape[0], ...]
+
+                train_data[source]["image.data"] = image
+
+            with ThreadPoolExecutor(
+                    max_workers=len(train_data.keys())) as executor:
+                for source in train_data.keys():
+                    executor.submit(_corrections, source)
+            # assemble image
+            try:
+                stacked_data = stack_detector_data(train_data, "image.data")
+            except (ValueError, IndexError, KeyError) as e:
+                print(e)
+                return
+            return stacked_data
 
     @classmethod
     def for_detector(cls, detector, geom_file=None, quad_prositions=None):
