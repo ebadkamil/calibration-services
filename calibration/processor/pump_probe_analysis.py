e3d9f6f23616c9efbbfa9237fade03e268d83dee
Ebad Kamil <ebad.kamil@xfel.eu>
2020-06-16 18:51:07 +0200

    Add processor in pump_probe_analysis

--- calibration/processor/pump_probe_analysis.py
+++ calibration/processor/pump_probe_analysis.py
@@ -29,6 +29,11 @@ class PumpProbeMode(IntEnum):
     SAME_TRAIN = 3
 
 
+class AnalysisType(IntEnum):
+    ROI = 1
+    AZIMUTHAL = 2
+
+
 def detector_data_collection(proposal, run, dettype):
     dettype = dettype.upper()
     assert dettype in ["AGIPD", "LPD", "JUNGFRAU"]
@@ -61,7 +66,80 @@ class PumpProbeAnalysis:
         "same_train": PumpProbeMode.SAME_TRAIN,
         })
 
-    def __init__(self, proposal, run, dettype, pp_mode):
+    _analysis_type = OrderedDict({
+        "roi": AnalysisType.ROI,
+        "azimuthal": AnalysisType.AZIMUTHAL,
+        })
+
+    def __init__(self, proposal, run, dettype, pp_mode, analysis_type):
+
         assert pp_mode in PumpProbeAnalysis._pp_mode.keys()
-        self.run = detector_data_collection(proposal, run, dettype)
+        assert analysis_type in PumpProbeAnalysis._analysis_type.keys()
+
+        self.dettype = dettype.upper()
+        assert self.dettype in ["AGIPD", "LPD", "JUNGFRAU"]
+
         self.pp_mode = PumpProbeAnalysis._pp_mode[pp_mode]
+        self.analysis_type = PumpProbeAnalysis._analysis_type[analysis_type]
+
+        self.run = detector_data_collection(proposal, run, dettype)
+        self.assembler =ImageAssembler.for_detector(dettype)
+        self.on = None
+        self.off = None
+        self.difference = None
+
+    def process(self, **kwargs):
+        if self.analysis_type == AnalysisType.ROI:
+            roi = kwargs.get("roi", None)
+            background = kwargs.get("bkg", None)
+            fom_type = kwargs.get("fom_type", None)
+
+            if roi is None or fom_type not in ["mean", "proj"]:
+                print(f"Check roi {roi} and fom_type {fom_type}")
+                return
+        train_ids = []
+        on = []
+        off = []
+        diff = []
+        for tid, data in self.run.trains():
+
+            assembled = self.assembler.assemble_image(data)
+            on_image, off_image = self._on_off_data(tid, assembled)
+
+            if on_image is not None and off_image is not None:
+                train_ids.append(tid)
+                if self.analysis_type == AnalysisType.ROI:
+                    x0, x1, y0, y1 = roi
+                    signal_on = on_image[..., x0:x1, y0:y1]
+                    signal_off = off_image[..., x0:x1, y0:y1]
+
+                    if background is not None:
+                        bx0, bx1, by0, by1 = background
+                        signal_on -= on_image[..., bx0:bx1, by0:by1]
+                        signal_off -= off_image[..., bx0:bx1, by0:by1]
+
+                    if fom_type == 'proj':
+                        on_fom = np.nanmean(signal_on, axis=-1)
+                        off_fom = np.nanmean(signal_off, axis=-1)
+
+                    else:
+                        on_fom = np.nanmean(signal_on, axis=(-1, -2))
+                        off_fom = np.nanmean(signal_off, axis=(-1, -2))
+
+                elif self.analysis_type == AnalysisType.AZIMUTHAL:
+                    pass
+
+                on.append(on_fom)
+                off.append(off_fom)
+                diff.append(on_fom - off_fom)
+
+        if trains:
+            coords = {'trainId': np.array(train_ids)}
+            dims = ['trainId', 'mem_cells'] + \
+                   [f'd{i}' for i in range(len(on.shape[1:]))]
+            self.on = xr.DataArray(
+                data=np.stack(on), dims=dims, coords=coords)
+            self.off = xr.DataArray(
+                data=np.stack(off), dims=dims, coords=coords)
+            self.diff = xr.DataArray(
+                data=np.stack(diff), dims=dims, coords=coords)
